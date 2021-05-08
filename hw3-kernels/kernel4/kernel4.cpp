6fa05331abfa845229ca5a0f92b7b9d9071a849a
Kushagra Shah <kushagrashah298@gmail.com>
2021-05-08 02:27:13 +0200

    Improve kernel2 - dependence; Optimize kernel4 - pipeline, rewrite

--- hw3-kernels/kernel4/kernel4.cpp
+++ hw3-kernels/kernel4/kernel4.cpp
@@ -2,8 +2,10 @@
 
 void kernel4(int array[ARRAY_SIZE], int index[ARRAY_SIZE], int offset)
 {
+    int sum = 0;
     loop:for (int i=offset+1; i<ARRAY_SIZE-1; ++i)
     {
-        array[offset] = array[offset]-index[i]*array[i]+index[i]*array[i+1];
+        sum = sum + index[i]*(array[i+1] - array[i]);
     }
+    array[offset] = array[offset] + sum;
 }
