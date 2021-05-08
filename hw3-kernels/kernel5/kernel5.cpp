1d7fa60260581742d26d3a5d74951eec58acf464
Kushagra Shah <kushagrashah298@gmail.com>
2021-05-08 17:47:26 +0200

    NoChange kernel5 - rewrite

--- hw3-kernels/kernel5/kernel5.cpp
+++ hw3-kernels/kernel5/kernel5.cpp
@@ -4,22 +4,59 @@ float kernel5(float bound, float a[ARRAY_SIZE], float b[ARRAY_SIZE])
 {
     int i=0;
     float sum;
-    float tmp[ARRAY_SIZE];
-    loop1:for (i=0; i<ARRAY_SIZE; i++)
-    {
-    	tmp[i] = a[i] + b[i];
-    }
-    i=0;
-	loop2:while (sum<bound && i<ARRAY_SIZE)
-    {
-        sum = tmp[i];
-        i++;
-    }
+//    float tmp[ARRAY_SIZE];
+//    loop1:for (i=0; i<ARRAY_SIZE; i++)
+//    {
+//    	tmp[i] = a[i] + b[i];
+//    }
+//    i=0;
+//	loop2:while (sum<bound && i<ARRAY_SIZE)
+//    {
+//        sum = tmp[i];
+//        i++;
+//    }
 
 //    loop:while (sum<bound && i<ARRAY_SIZE)
 //    {
 //        sum = a[i] + b[i];
 //        i++;
 //    }
-    return sum;
+
+//    loop: while (i<ARRAY_SIZE)
+//    {
+//    	float tmp = bound - b[i];
+//
+//    	if (a[i] >= tmp)
+//    	{
+//    		break;
+//    	}
+//
+//    	i++;
+//    }
+
+    float ans;
+    loop:for(i=0; i<ARRAY_SIZE; i++)
+    {
+    	sum = a[i] + b[i];
+
+    	if (sum >= bound)
+    	{
+    		ans = sum;
+    		break;
+    	}
+    }
+    return ans;
+
+//    bool pred = true;
+//
+//    loop:for (i=0; i<ARRAY_SIZE; i++)
+//    {
+//    	if (pred)
+//    	{
+//    		sum = a[i] + b[i];
+//    		pred = sum<bound;
+//    	}
+//    }
+
+//    return sum;
 }
